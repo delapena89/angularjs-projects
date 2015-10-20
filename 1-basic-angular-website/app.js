971a244e379a7d944f32a09628139a77137d9df9
Luis <delapenl@gmail.com>
2015-10-19 18:37:31 -0600

    linked js to html via ng-app, created first route using ngRoute

--- 1-basic-angular-website/app.js
+++ 1-basic-angular-website/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('computer');
+var app = angular.module('computer', [ngRoute])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/main',{
+      templateUrl: 'main.html',
+      controller: 'MainCtrl'
+  });
+}]);
