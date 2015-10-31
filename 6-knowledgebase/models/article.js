8b86f8439e6d5f8d49906fd1bb553669e2f95ac8
Luis <delapenl@gmail.com>
2015-10-31 15:00:46 -0600

    created methods to update and delete articles

--- 6-knowledgebase/models/article.js
+++ 6-knowledgebase/models/article.js
@@ -40,6 +40,37 @@ module.exports.getArticlesByCategory = function(category, callback) {
   Article.find(query, callback);
 };
 
+// Add an Article
+module.exports.createArticle = function(newArticle,callback){
+  newArticle.save(callback);
+};
+
+// Update Article
+module.exports.updateArticle = function(id, data, callback) {
+  var title     = data.title;
+  var body      = data.body;
+  var category  = data.category;
+
+  var query = {_id: id};
+
+  Article.findById(id, function(err, article){
+    if(!article){
+      return next(new Error('Could not load article'));
+    } else {
+      article.title     = title;
+      article.body      = body;
+      article.category  = category;
+
+      article.save(callback);
+    }
+  });
+};
+
+// Delete Article
+module.exports.removeArticle = function(id, callback){
+  Article.find({_id:id}).remove(callback);
+};
+
 
 
 
