86648bd1b4329b60e3e571e7f8568b21e839c730
Luis <delapenl@gmail.com>
2015-10-31 00:04:43 -0600

    added facebook skd, having hosting issues

--- 4-ngSocial-Facebook-App/app/facebook/facebook.js
+++ 4-ngSocial-Facebook-App/app/facebook/facebook.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngSocial.facebook', ['ngRoute'])
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/facebook', {
@@ -9,6 +9,40 @@ angular.module('ngSocial.facebook', ['ngRoute'])
   });
 }])
 
-.controller('FacebookCtrl', [function() {
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('443899752460567');
+  $facebookProvider.setPermissions("email","public_profile","user_posts","publish_actions","user_photos");
+})
 
+.run(function($rootScope){
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+})
+
+.controller('FacebookCtrl', ['$scope','$facebook',function($scope, $facebook) {
+  $scope.isLoggedIn = false;
+  $scope.login = function() {
+    $facebook.login().then(function(){
+      console.log('Logged In');
+    });
+  };
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
