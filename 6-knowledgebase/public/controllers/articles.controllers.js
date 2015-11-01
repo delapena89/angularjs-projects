38f20f647d78d1663c86a571d223a3c577ceab5e
Luis <delapenl@gmail.com>
2015-10-31 19:57:14 -0600

    finished add article functionality

--- 6-knowledgebase/public/controllers/articles.controllers.js
+++ 6-knowledgebase/public/controllers/articles.controllers.js
@@ -19,10 +19,24 @@ angular.module('kB')
   });
 }])
 
-.controller('ArticlesCreateCtrl', ['$scope', '$http','$routeParams', '$location', function($scope, $http, $routeParams, $location){
+.controller('ArticleCreateCtrl', ['$scope', '$http','$routeParams', '$location', function($scope, $http, $routeParams, $location){
   $http.get('/categories').success(function(data){
     $scope.categories = data;
   });
+
+  $scope.addArticle = function(){
+    var data = {
+      title: $scope.title,
+      body: $scope.body,
+      category: $scope.category
+    };
+
+    $http.post('/articles', data).success(function(data, status){
+        console.log(status);
+    });
+
+    $location.path('/articles');
+  };
 }]);
 
 
